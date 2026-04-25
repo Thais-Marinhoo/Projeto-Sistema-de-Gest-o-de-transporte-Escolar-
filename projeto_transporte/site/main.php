11e0d01f673d5d3df9aefe1197f122481eb24841
nayraamandad <nayra.soares2@aluno.ce.gov.br>
2026-04-25 07:56:52 -0300

    correcoes no visual da tela admin+tela editar

---
+++ projeto_transporte/site/main.php
@@ -0,0 +1,46 @@
+<?php
+include ('menu.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Dashboard</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="mstyle.css">
+
+</head>
+
+<body>
+
+
+<div class="conteudo">
+
+    <h1>Dashboard</h1>
+
+    <div class="cards">
+        <div class="card">
+            <p>Total de alunos</p>
+            <h2>350</h2>
+        </div>
+
+        <div class="card">
+            <p>Total de rotas</p>
+            <h2>9</h2>
+        </div>
+
+        <div class="card">
+            <p>Ônibus ativos</p>
+            <h2>15</h2>
+        </div>
+    </div>
+
+    <div class="mapa">
+        Aqui vai o mapa depois
+    </div>
+
+</div>
+
+</body>
+</html>
